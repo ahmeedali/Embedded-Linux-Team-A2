a5a06d42a49d061aead4e47ad2d569cac79754ac
Omar Hosny <omarhosny58@gmail.com>
2023-08-10 14:32:23 +0300

    vowel check task first commit

---
+++ omar/python/week01/vowel_check.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+vowelList = ['a','e','i','o','u','A','E','I','O','U']
+
+c = input("Please enter a letter: ")
+
+if c not in vowelList:
+    print("That's not a Vowel\n")
+else:
+    print("That's a Vowel\n")
