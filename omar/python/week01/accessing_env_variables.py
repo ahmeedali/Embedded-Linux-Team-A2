82e4bdf1717ed851f9d30b25754b4c041c335bdb
Youssef Adel Youssef <youssefadel1502@gmail.com>
2023-08-10 21:07:54 +0300

    Remove Omar Directory File

--- omar/python/week01/accessing_env_variables.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-import os
-
-for key in os.environ:
-    print("\n" + os.environ[key])
--- omar/python/week01/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import datetime
-
-now = datetime.datetime.now()
-
-print(str(now)[0:-7])
