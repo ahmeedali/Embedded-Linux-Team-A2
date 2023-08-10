b78ac0d69bc519d30670e1edd4cca72d73f12575
Youssef Adel Youssef <youssefadel1502@gmail.com>
2023-08-10 21:06:38 +0300

    Remove Ahmed Directory File

--- ahmed/python/week01/vowel-or-const-test.py
+++
@@ -1,8 +0,0 @@
-letter = input ( " enter a letter of alphabet : ")
-
-if letter in  ('a', 'e', 'i', 'o', 'u'):
-      print (" %s is a vowel " % letter )
-elif letter == 'y':
-      print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
-else:
-      print("%s is a consonant." % letter )  
