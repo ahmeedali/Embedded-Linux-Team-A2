dc9e326a1a237818e66e4257efe1b65fea8ea9a0
ahmeedali <ahmed.mohamed.abuelhassan@gmail.com>
2023-08-12 16:29:52 +0300

    deleted

--- ahmed/python/week01/01-vowel-or-const-test.py
+++
@@ -1,8 +0,0 @@
-
-
-letter = input ( " enter a letter of alphabet : ")
-
-if letter in  ('a', 'e', 'i', 'o', 'u'):
-      print (" %s is a vowel " % letter )
-else:
-      print("%s is a consonant." % letter )  
