4f891b60f23f16eb77047d8ca233bc203b0cbf18
ahmeedali <ahmed.mohamed.abuelhassan@gmail.com>
2023-08-22 02:53:36 +0300

    will coplete som feature

---
+++ ahmed/python/week01/13-Your-location.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import requests
+
+url = requests.get ("https://api.ipify.org/?format=json")
+ip  = url.json()["ip"]
+url2= requests.get(f"https://api.iplocation.net/?ip={ip}")
+print(url2.json())
---
+++ ahmed/python/week01/14-Install-VScode-Extentions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from time import *
+import pyautogui as gui
+gui.confirm('Shall we proceed?')
+sleep(1)
+gui.hotkey('win')
+sleep(0.5)
+gui.write("terminator")
+sleep(1)
+gui.hotkey("enter")
+sleep(0.5)
+gui.write("code")
+sleep(0.5)
+gui.hotkey("enter")
+sleep(3)
+gui.hotkey('ctrl', 'shift', 'x')
+sleep(0.5)
+gui.hotkey("f11")
+sleep(0.5)
+x,y=gui.position()
+print(x,y)
+gui.click(130, 51, 1, button='left')
+gui.write("code")
+x, y = gui.locateCenterOnScreen('calc7key.png')
+gui.click(x, y)
+
