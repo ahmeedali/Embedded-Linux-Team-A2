90a74e4763e5cefa8f1f5c5c5f6ce82eeba9b712
ahmeedali <ahmed.mohamed.abuelhassan@gmail.com>
2023-08-22 02:53:36 +0300

    will coplete som feature

---
+++ ahmed/python/week01/12-Suggest-activates.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import requests
+
+print ("Choose one or more activity of 10 activities")
+for i in range(10):
+    url = requests.get ("https://www.boredapi.com/api/activity")
+    print("- " + url.json()['activity'])
+print ("\n Enjoy ^_^ \n")    
