0956c97e3473d2661cc0f1e80ee5ed6fab2a4451
ahmeedali <ahmed.mohamed.abuelhassan@gmail.com>
2023-09-02 16:00:57 +0300

    Create server receive multiple clients and keep alive using sokect

--- ahmed/python/week02/07-Multithreaded-Server/01-Client.py
+++ ahmed/python/week02/07-Multithreaded-Server/01-Client.py
@@ -3,15 +3,21 @@
 import socket
 # Socket connection(IP + port)
 client1=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client1.connect(('127.0.1.1',5050)) 
+ip = socket.gethostbyname(socket.gethostname())
+client1.connect((ip,8888)) 
  
 print ("="*50)
+
 while True:
-    msg=input("Please enter message :")
-    client1.send(msg.encode('UTF-8'))
+    msg=str(input("Please enter message :"))
+    msg_encode= msg.encode('UTF-8')
+    client1.send(msg_encode)
     print ("="*50)
     data=client1.recv(1024)
     print(f"you recevied message :{data.decode('UTF-8')}")
-    # if data.decode('UTF-8')=="bye":
-    #     break
-    client1.close()
+    print ("="*50)
+    if data.decode('UTF-8')=="bye":
+        client1.close()
+        break
+    else :
+        continue
