49a83eeb4ff6a091f645edfba35af739099a5457
ahmeedali <ahmed.mohamed.abuelhassan@gmail.com>
2023-09-17 01:45:06 +0300

    summation the digits of integer entered by user

---
+++ ahmed/c++/session_01/07-Sum-digit-int-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+  int n, sum{0};
+  std::cout << "Enter a number: ";
+  std::cin >> n;
+  while (n > 0) {
+
+    sum += n % 10;
+    n /= 10;
+  }
+  std::cout << "Sum is = " << sum << std::endl;
+  return 0;
+}
