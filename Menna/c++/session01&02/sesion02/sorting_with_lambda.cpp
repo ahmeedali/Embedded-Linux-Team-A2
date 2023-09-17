55860e62ea5c9faa9eb26697a46c2eb2327c5c57
Menna Sayed <menna604.ms@gmail.com>
2023-09-18 00:01:07 +0300

    Create sorting_with_lambda.cpp

---
+++ Menna/c++/session01&02/sesion02/sorting_with_lambda.cpp
@@ -0,0 +1,21 @@
+auto lam_sort_ascend = [](const int a,const int b){
+
+   return (a < b);
+};
+
+auto lam_sort_descend = [](const int a,const int b){
+
+   return (a > b);
+};
+
+int main() 
+{
+     std::array<int,5> arr={5,2,4,3,1};
+     std::sort(arr.begin(),arr.end(),lam_sort_ascend);   //using lambda function for custom sorting
+
+     for(int i : arr)
+      std::cout<<i<<" ";
+
+   std::cout<<std::endl;
+   return 0;
+}
