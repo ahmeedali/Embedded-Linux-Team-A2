4f48a0848cf7b9215063a99f2a1f94256a001aad
Menna Sayed <menna604.ms@gmail.com>
2023-09-18 00:13:39 +0300

    Create Searching_for_element.cpp

---
+++ Menna/c++/session01&02/sesion02/Searching_for_element.cpp
@@ -0,0 +1,29 @@
+template <std::size_t array_size>                  // generic array size type using template
+bool search_ele(int (&arr)[array_size],int no){   //passing a refrence array and its size to make ranged for loop work inside a function 
+
+    for(int i : arr){   
+      if (i == no ) {
+       return true;
+       }
+    }
+ return false;
+}
+
+int main()
+{
+   int no=0;
+   int arr[10]={1,2,9,20,3,80,100,66,52,2};
+  
+   std::cin>>no;
+   if(search_ele(arr,no))
+   {
+      std::cout<<"the no is found"<<std::endl;
+   }
+   else
+    {
+      std::cout<<"the no isn't found"<<std::endl;
+    }
+   
+  
+return 0;
+}
